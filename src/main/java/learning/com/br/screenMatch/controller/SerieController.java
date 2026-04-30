e791faf356d86927b00db8884c91a67412b99c97
IterbioArantes <iterbio7@gmail.com>
2026-04-29 22:25:34 -0300

    Adding Spring Web

---
+++ src/main/java/learning/com/br/screenMatch/controller/SerieController.java
@@ -0,0 +1,13 @@
+package learning.com.br.screenMatch.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SerieController {
+
+    @GetMapping("/series")
+    public String obterSeries(){
+        return "Test";
+    }
+}
